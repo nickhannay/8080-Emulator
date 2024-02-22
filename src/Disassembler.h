a09cf9b624fed0611dd148bb9e2ea5b96111a974
Nick Hannay <nickhannay@outlook.com>
2024-02-21 22:19:15 -0800

    dissassembler now ouputs number of cycles executed

--- src/Disassembler.h
+++ src/Disassembler.h
@@ -2,7 +2,7 @@
 #define _DISASSEMBLER_H
 #include "data_types.h"
 
-int printOpCode(byte codes[], int index);
+int printOpCode(byte codes[], int index, uint64_t int_num);
 
 
 int processBuffer(byte codes[], int buffer_size);
